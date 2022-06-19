b964450c01f7b841ad2b9ef752553ca15fc417c3
dustinc555 <dckook10@gmail.com>
2022-06-18 22:41:55 -0600

    compute new id for new appointments

--- example_api/db/static/physicians.py
+++ example_api/db/static/physicians.py
@@ -10,7 +10,12 @@ logger = logging.getLogger()
 def find_physician_by_id(physicians, physician_id):
         return next((index for index, physician in enumerate(physicians) if str(physician['_id']) == str(physician_id)), -1)
 
+def compute_next_id(list_of_items = []):
+    """computes next logical _id for list of items with _id"""
+    return max([-1] + [int(x['_id']) for x in  list_of_items]) + 1
+
 class StaticDB():
+    """Primary interface for the static physicians database"""
 
     def __init__(self, path):
         self.path = '%s/physicians.json' % path
@@ -23,7 +28,7 @@ class StaticDB():
             f.write(to_json(physicians, pretty_print=True))
 
     def get_physician_appointments(self, physician_id):
-        physicians = json.load(open(self.path))
+        physicians = self.get_all_physicians()
 
         index = find_physician_by_id(physicians, physician_id)
 
@@ -33,13 +38,20 @@ class StaticDB():
         return physicians[index]['appointments']
 
     def add_physician_appointments(self, physician_id, new_appointments):
-        physicians = json.load(open(self.path))
+
+        logger.debug('adding new appointments for physician %s' % physician_id)
+        logger.debug(str(new_appointments))
+
+        physicians = self.get_all_physicians()
 
         index = find_physician_by_id(physicians, physician_id)
 
         if index == -1:
             raise NotFoundError('Could not find physician with id %s' % physician_id)
 
-        physicians[index]['appointments'] += new_appointments
+        # server side compute each new id for appointments
+        for appointment in new_appointments:
+            appointment["_id"] = compute_next_id(physicians[index]['appointments'])
+            physicians[index]['appointments'].append(appointment)
 
         self.write_physicians(physicians)
