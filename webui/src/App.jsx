e12109f761d706bc945ab7f6e18003072361c425
dustinc555 <dckook10@gmail.com>
2022-06-19 00:38:54 -0600

    replaced DataGrid with Table for more freedom

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -1,5 +1,15 @@
 import Box from "@mui/material/Box";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#337FDD",
+    },
+  },
+});
+
 import { useState } from "react";
 import { usePhysicians } from "./services/physicians";
 
@@ -12,14 +22,16 @@ export default function App() {
 
   if (!isLoading) {
     return (
-      <Box sx={{ display: "flex", flexDirection: "row", p: 1, m: 1 }}>
-        <PhysiciansList
-          physicians={physicians}
-          selectedIndex={selectedIndex}
-          setSelectedIndex={setSelectedIndex}
-        />
-        <Calendar {...physicians[selectedIndex]} />
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: "flex", flexDirection: "row", p: 1, m: 1 }}>
+          <PhysiciansList
+            physicians={physicians}
+            selectedIndex={selectedIndex}
+            setSelectedIndex={setSelectedIndex}
+          />
+          <Calendar {...physicians[selectedIndex]} />
+        </Box>
+      </ThemeProvider>
     );
   } else return <h1>Loading...</h1>;
 }
--- webui/src/components/Calendar.jsx
+++ webui/src/components/Calendar.jsx
@@ -1,5 +1,28 @@
-import { DataGrid } from "@mui/x-data-grid";
-import { Typography, Stack, Box } from "@mui/material";
+import {
+  Typography,
+  Stack,
+  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  tableCellClasses,
+  TableBody,
+  Paper,
+} from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
 import AddComponentForm from "./AddAppointmentForm";
 
@@ -9,40 +32,39 @@ export default function Calendar({
   email = "",
   _id = "",
 }) {
-  const columns = [
-    { field: "id", headerName: "#", width: 50 },
-    { field: "name", headerName: "Name", width: 130 },
-    {
-      field: "time",
-      headerName: "Time",
-      width: 160,
-      type: "dateTime",
-    },
-    { field: "kind", headerName: "Kind", width: 130 },
-  ];
-
-  const rowsprop = appointments.map((r) => ({
-    ...r,
-    id: r._id,
-  }));
+  const rowsprop = appointments.sort(
+    (a, b) => new Date(b.time) - new Date(a.time)
+  );
 
   return (
     <Stack spacing={2} sx={{ pl: 2, width: "100%" }}>
       <Typography variant="h4">{name}</Typography>
       <Typography variant="h6">{email}</Typography>
-      <DataGrid
-        paging={true}
-        sx={{ minHeight: 300 }}
-        emptyRowsWhenPaging={false}
-        pageSizeOptions={[6, 12, 20, 50]}
-        rows={rowsprop}
-        columns={columns}
-        initialState={{
-          sorting: {
-            sortModel: [{ field: "time", sort: "asc" }],
-          },
-        }}
-      />
+      <TableContainer sx={{ maxHeight: 440 }} component={Paper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="right">#</StyledTableCell>
+              <StyledTableCell align="right">Name</StyledTableCell>
+              <StyledTableCell align="right">Time</StyledTableCell>
+              <StyledTableCell align="right">Kind</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rowsprop.map((row) => (
+              <TableRow
+                key={row._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="right">{row._id}</TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.time}</TableCell>
+                <TableCell align="right">{row.kind}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       {_id !== "" ? (
         <Box sx={{ display: "flex", flexDirection: "row" }}>
           <AddComponentForm physician_id={_id} />
