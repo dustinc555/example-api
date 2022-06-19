b93287d91c902a809014d6a7b2c0f3412116793b
dustinc555 <dckook10@gmail.com>
2022-06-18 22:31:36 -0600

    added base resolve path
    its so that @ references /src

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,4 +12,9 @@ export default defineConfig({
       registerType: "autoUpdate",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
